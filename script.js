ca9ed637ce9ec1720a71ae1dfbc36f419b004761
Sherreka <rekadarnb@gmail.com>
2022-05-30 11:23:46 -0400

    add javascript elements

---
+++ script.js
@@ -0,0 +1,23 @@
+// DOM 
+const cellElements = document.querySelectorAll('[data-cell');
+const boardElements = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.getElementById('winnngMessageText')
+
+
+
+const PLAYER_X_CLASS = 'x';
+const PLAYER_O_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+let isPlayer_O_Turn = false;
