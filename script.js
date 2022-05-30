6af7ce62be4a19077f501ccfd8e9162df1854946
Sherreka <rekadarnb@gmail.com>
2022-05-30 12:02:14 -0400

    fixed styling issue

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // DOM 
-const cellElements = document.querySelectorAll('[data-cell');
-const boardElements = document.getElementById('board');
+const cellElements = document.querySelectorAll('[data-cell]');
+const boardElement = document.getElementById('board');
 const winningMessageElement = document.getElementById('winningMessage');
 const restartButton = document.getElementById('restartButton');
 const winningMessageTextElement = document.getElementById('winnngMessageText')
 
+let isPlayer_O_Turn = false;
 
 // game play elements 
 const PLAYER_X_CLASS = 'x';
@@ -19,8 +20,6 @@ const WINNING_COMBINATIONS = [
     [0,4,8],
     [2,4,6]
 ]
-
-let isPlayer_O_Turn = false;
 
 startGame();
 
@@ -60,4 +59,36 @@ function endGame(draw) {
         winningMessageTextElement.innerText = `Player ${isPlayer_O_Turn ? "O" : "X"} wins!`
     }
     winningMessageElement.classList.add('show')
-}
+};
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS);
+    });
+};
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+};
+
+function swapTurns(){
+    isPlayer_O_Turn = !isPlayer_O_Turn;
+};
+
+function setBoardHoverClass(){
+    boardElement.classList.remove(PLAYER_X_CLASS);
+    boardElement.classList.remove(PLAYER_O_CLASS);
+    if (isPlayer_O_Turn){
+        boardElement.classList.add(PLAYER_O_CLASS);
+    } else {
+        boardElement.classList.add(PLAYER_X_CLASS);
+    }
+};
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+};
